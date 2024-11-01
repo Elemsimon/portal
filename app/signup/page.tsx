1a8209709030281b2be0b19af9aa7e207ad828cd
Elemsimon <elemifesinachisimon@gmail.com>
2024-11-01 14:12:01 +0100

    feat singup

---
+++ app/signup/page.tsx
@@ -0,0 +1,34 @@
+import LoginForm from '@/components/loginForm'
+import SignupForm from '@/components/signupForm'
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex flex-col sm:flex-row justify-start h-screen overflow-hidden">
+      <div className="flex-shrink-0 items-center justify-items-center py-8 sm:py-16 pb-20 gap-16 px-8 sm:px-10 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Image
+            className="dark:invert"
+            src="/logo.png"
+            alt="Next.js logo"
+            width={400}
+            height={38}
+            priority
+          />
+          <SignupForm />
+        </main>
+      </div>
+
+      {/* Background section filling the viewport with bg-top and no scroll */}
+      <div 
+        className="w-full h-screen bg-top bg-cover sm:bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('/bg.png')`,
+        }}
+      ></div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/signupForm.tsx
@@ -0,0 +1,115 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, {
+        message: "This field has to be filled.",
+      })
+      .email("This is not a valid email")
+      .max(300, {
+        message: "Password can't be longer than 300 characters.",
+      }),
+    password: z
+      .string()
+      .min(6, { message: "Password has to be at least 6 characters long." }),
+    confirmPassword: z.string().min(6, {
+      message: "Confirm-Password has to be at least 6 characters long.",
+    }),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      });
+    }
+  });
+
+const SignupForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div className="w-full">
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <h1 className="text-2xl font-bold">Registration</h1>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@whatever.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <p> Already have an account? &nbsp;
+          <Link className="text-slate-800 hover:text-blue-500" href={"/login"}>
+            Login
+          </Link>
+        </p>
+        <Button type="submit" className="w-full hover:bg-pink-600 p-6 text-lg font-bold">Submit</Button>
+      </form>
+    </Form>
+    </div>
+  )
+}
+
+export default SignupForm
